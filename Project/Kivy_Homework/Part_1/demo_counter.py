f7eaa5ca1f43d5a55869beae4fd047f6104d374c
Alsa2 <alsa0622@hotmail.com>
2023-01-31 14:47:42 +0900

    added part 1

--- Project/Kivy_Homework/Part_1/demo_counter.py
+++ Project/Kivy_Homework/Part_1/demo_counter.py
@@ -1,17 +1,20 @@
 from kivymd.app import MDApp
 
 class layout_demo(MDApp):
-    def build(self):
-        return
-    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.original = 0
         self.output = 0
 
-    def currency_converter(self, currency:str):
-        pass
 
+    def build(self):
+        return
+
+    def convert_currency(self, choice:str):
+        if 'USD' in choice:
+                self.output = float("{:.2f}".format(int(self.root.ids.original_in.text)*1.09))
+        elif 'JPY' in choice:
+            self.output = float("{:.2f}".format(int(self.root.ids.original_in.text)*141))
 
 
 demo_class = layout_demo()
