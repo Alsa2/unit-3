dd95cc54ee021712358be4da213d7745216149dd
Alsa2 <alsa0622@hotmail.com>
2023-01-31 15:05:53 +0900

    corrected task 1

--- Project/Kivy_Homework/Part_1/demo_counter.py
+++ Project/Kivy_Homework/Part_1/demo_counter.py
@@ -15,6 +15,10 @@ class layout_demo(MDApp):
                 self.output = float("{:.2f}".format(int(self.root.ids.original_in.text)*1.09))
         elif 'JPY' in choice:
             self.output = float("{:.2f}".format(int(self.root.ids.original_in.text)*141))
+        else:
+            self.output = "Invalid Currency"
+
+        self.root.ids.final_label.text = f"{self.output} {choice}"
 
 
 demo_class = layout_demo()
