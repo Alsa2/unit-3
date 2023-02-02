c4daf36029391939a8e847bdd654afb597026d06
Alsa2 <alsa0622@hotmail.com>
2023-02-02 15:03:06 +0900

    finished login

--- Project/Login_kivy/login.py
+++ Project/Login_kivy/login.py
@@ -1,26 +1,39 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-import csv
 
 csv_path = "/Users/alsa/Documents/unit-3/Project/Login_kivy/accounts.csv"
 
 class LoginScreen(MDScreen):
     def login(self, username, password):
         
-        with open(csv_path, "r") as csv_file:
-            reader = csv.reader(csv_file, delimiter=",")
-            
-        for row in reader:
-            if username == row[0] and password == row[1]:
-                self.root.current = "login_success"
+        with open(csv_path, "r") as file:
+            users = file.readlines()
+
+        for user in users:
+            if user == f"{username},{password}":
+                self.manager.current = "success"
                 return
             else:
-                self.root.ids.error_label.text = "Invalid username or password"
+                self.ids.error_label.text = "Invalid username or password"
+                self.ids.password.helper_text_mode = "on_error"
+                self.ids.password.helper_text = "Invalid password"
+                print("Invalid username or password")
 
 
 
 class RegistrationScreen(MDScreen):
-    pass
+    def register(self, username, password, confirm_password):
+        if password == confirm_password:
+            with open(csv_path, "a") as file:
+                file.write(f"{username},{password}\n")
+                self.manager.current = "success"
+        else:
+            self.ids.error_label.text = "Passwords do not match"
+            self.ids.password.helper_text_mode = "on_error"
+            self.ids.password.helper_text = "Passwords do not match"
+            self.ids.confirm_password.helper_text_mode = "on_error"
+            self.ids.confirm_password.helper_text = "Passwords do not match"
+
 
 class LoginSuccess(MDScreen):
     pass
