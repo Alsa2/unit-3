2534925cd7ef5cabf1865c3f816e4b5908fe12a4
Alsa2 <alsa0622@hotmail.com>
2023-02-03 11:28:30 +0900

    put capital letter on classseeeessss

--- Quizzes/Quizz-42/mystery.py
+++ Quizzes/Quizz-42/mystery.py
@@ -2,11 +2,11 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 
 
-class mysteryPageA(MDScreen):
+class MysteryPageA(MDScreen):
     def message1(self):
         print("This is mystery page A you pressed the button")
 
-class mysteryPageB(MDScreen):
+class MysteryPageB(MDScreen):
     def message2(self):
         print("This is mystery page B you pressed the button")
 
