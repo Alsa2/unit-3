0c83207e81a92a710bd84a1bcf4a9d84e82d52b0
Alsa2 <alsa0622@hotmail.com>
2023-03-14 10:26:23 +0900

    update

--- Quizzes/Quizz-48/script.py
+++ Quizzes/Quizz-48/script.py
@@ -1,49 +1,38 @@
-import sqlite3
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker
-from secure_password import encrypt_password, check_password
+from secure_password import check_password
+
 
 red = "\33[0;31m"
 green = "\33[0;32m"
 end = "\33[0m"
 
-class WalletHandler():
-    def __init__(self):
-        self.connection = sqlite3.connect("Quizzes/Quizz-48/bitcoin_exchange.db")
-        self.cursor = self.connection.cursor()
-    
-    def search(self, query):
-        result = self.cursor.execute(query).fetchall()
-        return result
+Base = declarative_base()
 
-    def run_save(self, query):
-        self.cursor.execute(query)
-        self.connection.commit()
+class Ledger(Base):
+    __tablename__ = 'ledger'
+    id = Column(Integer, primary_key=True)
+    sender_id = Column(Integer, nullable=False)
+    receiver_id = Column(Integer, nullable=False)
+    amount = Column(Integer, nullable=False)
+    signature = Column(String, nullable=False)
 
-    def close(self):
-        self.connection.close()
-    
-    def get_ids(self):
-        ids = self.search("SELECT id FROM ledger")
-        return ids
+    def __repr__(self):
+        return f"id {self.id},sender_id {self.sender_id},receiver_id {self.receiver_id},amount {self.amount},hash={self.signature}"
 
-handler = WalletHandler()
-ids = handler.get_ids()
-for id in ids:
-    # id is the id of the transaction
-    # get the transaction
-    transaction = handler.search(f"SELECT * FROM ledger WHERE id={id[0]}")
-    print(transaction)
-    x = transaction[0]
-    # get sender_id of the transaction
-    sender_id = transaction[0][1]
-    # get receiver_id of the transaction
-    receiver_id = transaction[0][2]
-    # get amount of the transaction
-    amount = transaction[0][3]
-    # get the hash of the transaction
-    hash = transaction[0][4]
-
+engine = create_engine("sqlite:///Quizzes/Quizz-48/bitcoin_exchange.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+rows = session.query(Ledger).all()
+print(rows)
+for row in rows:
+    x = row.id
+    sender_id = row.sender_id
+    receiver_id = row.receiver_id
+    amount = row.amount
+    hash = row.signature
     unhashed = f"id {x},sender_id {sender_id},receiver_id {receiver_id},amount {amount}"
     if check_password(hash, unhashed):
         print(f"{green}Tx(id={x})Signature Matches{end}")
@@ -55,3 +44,4 @@ for id in ids:
 
 
 
+ 
